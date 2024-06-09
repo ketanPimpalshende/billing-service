30ac0c7e54081c496be4e8e5a4e0ffd0e448f428
Ketan Pimpalshende <ketanpimpalshende@gmail.com>
2024-06-09 13:42:43 +0530

    initial setup

---
+++ src/test/java/com/root/billing/BillingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.root.billing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
